9393a613aa769b6425913c5bd71e26270bb7df31
Dennise Camacho <dennise.e.camacho@gmail.com>
2025-08-26 15:30:37 -0500

    minor detail change, note about where to find repo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Todo list </p>
+        <br/>repository found at : <a href="https://github.com/DenniseCamacho/5.2GP">https://github.com/DenniseCamacho/5.2GP</a>
+        <br/>
         <TodoList />
       </header>
     </div>
